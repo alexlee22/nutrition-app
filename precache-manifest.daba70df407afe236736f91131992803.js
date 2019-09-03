d683dbbc6b0eb96805c144d3a94d35fd127b0c35
Alexander Lee <10814755+alexlee22@users.noreply.github.com>
2019-09-03 17:08:26 +1000

    Updates

---
+++ precache-manifest.daba70df407afe236736f91131992803.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee63a9d2a6c707af96c76bed5900e9a8",
+    "url": "/nutrition-app/index.html"
+  },
+  {
+    "revision": "3645b2729e1463b21f32",
+    "url": "/nutrition-app/static/js/2.bde329ee.chunk.js"
+  },
+  {
+    "revision": "1b517d5dbe4958cce613",
+    "url": "/nutrition-app/static/js/main.e6fce4a7.chunk.js"
+  },
+  {
+    "revision": "06d9bb8632241b196099",
+    "url": "/nutrition-app/static/js/runtime~main.7a66ffc0.js"
+  }
+]);
